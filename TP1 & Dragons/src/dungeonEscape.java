bc166320350f46a0497ea837e3f3c1b0e5fdedbf
Pedro Daniel dos Santos Reis <petrexkgb@gmail.com>
2017-02-14 18:29:55 +0000

    Funciona o guarda

--- TP1 & Dragons/src/dungeonEscape.java
+++ TP1 & Dragons/src/dungeonEscape.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class dungeonEscape {
 	public static void main(String[] args ){
 		boolean won = false, lost = false;
-		System.out.println("Welcome to Escape The Dungeon!\n To make your hero move, here's what you need to type.\nU - up;\nR - right;\nL - left;\nD - down;\n\nHave fun!\n");
+		System.out.println("Bem-vindo ao Dungeon Escape!\n Para fazer o teu herói mexer, isto é o que precisas de saber.\nU - cima;\nR - direita;\nL - esquerda;\nD - baixo;\n\nBoa sorte!\n");
 		Scanner scan = new Scanner(System.in);
 		char[][] a = {{'X','X','X','X','X','X','X','X','X','X'},{'X','H',' ',' ','I',' ','X',' ','G','X'},{'X','X','X',' ','X','X','X',' ',' ','X'},{'X',' ','I',' ','I',' ','X',' ',' ','X'},{'X','X','X',' ','X','X','X',' ',' ','X'},{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},{'X','X','X',' ','X','X','X','X',' ','X'},{'X',' ','I',' ','I',' ','X','K',' ','X'},{'X','X','X','X','X','X','X','X','X','X'}};
 		boolean finish = game(a);
 		if (finish == true) {
-			System.out.println("Ganhei o jogo, e acabei de perder o jogo");
+			System.out.println("Ganhei o jogo, e acabei de perder o jogo.");
 		}
+		if (!finish) 
+			System.out.println("Acabei efetivamente de perder o jogo.");
 	}
 
 	public static boolean game(char[][] a) {
-		boolean won = false, lost = false;
+		boolean won = false, lost = false;;
 		Scanner scan = new Scanner(System.in);
 		
 		showMap(a);
@@ -22,12 +24,35 @@ public class dungeonEscape {
 			
 			if (a[5][0] == 'H' || a[6][0] == 'H')
 				won = true;
+			if (checkPresence(a)) {
+				lost = true;
+				won = false;
+			}
 			
 		} while (!won && !lost);
 		return won;
 	}
 	
-
+	public static boolean checkPresence(char[][] a) {
+		int row = 0, col = 0, rowg = 0,colg = 0;
+		for (int i = 0 ; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				if (a[i][j] == 'H') {
+					row = i; col = j;
+				}
+				if (a[i][j] == 'G') {
+					rowg = i;
+					colg = j;
+				}
+			}
+		}
+		
+		if (colg == col && (rowg == row-1 || rowg == rowg+1 ))
+			return true;
+		if (rowg == row && (colg == col-1 || colg == col+1 ))
+			return true;
+		return false;
+	}
 	
 	public static char[][] move(char[][] a, char c) {
 		boolean won = false;
@@ -107,9 +132,51 @@ public class dungeonEscape {
 				}
 			}
 		}
+		a = moveGuard(a);
 		showMap(a);
 		return a;
 
+	}
+	
+	public static char [][] moveGuard(char[][] a) {
+		int rowg = 0, colg = 0;
+		for (int i = 0 ; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				if (a[i][j] == 'G') {
+					rowg = i;
+					colg = j;
+				}
+			}
+		}
+		
+		if (rowg == 1 && colg == 8) {
+			a[rowg][colg] = ' ';
+			a[rowg][colg-1] = 'G';
+		}
+		else if (rowg >= 1 && colg == 7 && rowg < 5) {
+			a[rowg][colg] = ' ';
+			a[rowg+1][colg] = 'G';
+		}
+		else if (rowg == 5 && colg <= 7 && colg >1) {
+			a[rowg][colg] = ' ';
+			a[rowg][colg-1] = 'G';
+		}
+		else if (rowg == 5 && colg == 1) {
+			a[rowg][colg] = ' ';
+			a[rowg+1][colg] = 'G';
+		}
+		else if (rowg == 6 && colg >= 1 && colg < 8) {
+			a[rowg][colg] = ' ';
+			a[rowg][colg+1] = 'G';
+		}
+		else if (rowg > 1 && rowg <= 6 && colg == 8) {
+			a[rowg][colg] = ' ';
+			a[rowg-1][colg] = 'G';
+		}
+			
+		
+		return a;
+		
 	}
 
 
