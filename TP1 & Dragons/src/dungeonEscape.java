9b8df3618a186c95ada157527ccd34d5c625ae74
Pedro Daniel dos Santos Reis <petrexkgb@gmail.com>
2017-02-14 15:40:23 +0000

    Inicio das mapa

---
+++ TP1 & Dragons/src/dungeonEscape.java
@@ -0,0 +1,9 @@
+
+public class dungeonEscape {
+	public static void main(String[] args ){
+		char[][][][][][][][][][] a = {{'X','X','X','X','X','X','X','X','X','X'},{'X','X','X','X','X','X','X','X','X','X'}};
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+}
