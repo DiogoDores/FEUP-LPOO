e36632d6bea6ddffed5f2b44456e554984544bed
Diogo Dores <diogo.dores@gmail.com>
2017-02-20 14:40:12 +0000

    Ogre a andar randomly

--- TP1 & Dragons/src/dungeonEscape.java
+++ TP1 & Dragons/src/dungeonEscape.java
@@ -1,65 +1,123 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class dungeonEscape {
+
+	public static boolean isLevelTwo = false;
+
 	public static void main(String[] args ){
 		boolean won = false, lost = false;
-		System.out.println("Bem-vindo ao Dungeon Escape!\n Para fazer o teu herói mexer, isto é o que precisas de saber.\nU - cima;\nR - direita;\nL - esquerda;\nD - baixo;\n\nBoa sorte!\n");
+		System.out.println("Welcome to Dungeon Escape!\nTo make your hero move, use the WASD keys.\n\nGood luck!\n");
 		Scanner scan = new Scanner(System.in);
-		char[][] a = {{'X','X','X','X','X','X','X','X','X','X'},{'X','H',' ',' ','I',' ','X',' ','G','X'},{'X','X','X',' ','X','X','X',' ',' ','X'},{'X',' ','I',' ','I',' ','X',' ',' ','X'},{'X','X','X',' ','X','X','X',' ',' ','X'},{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},{'X','X','X',' ','X','X','X','X',' ','X'},{'X',' ','I',' ','I',' ','X','K',' ','X'},{'X','X','X','X','X','X','X','X','X','X'}};
-		boolean finish = game(a);
+
+		char[][] a = {{'X','X','X','X','X','X','X','X','X','X'},
+				{'X','H',' ',' ','I',' ','X',' ','G','X'},
+				{'X','X','X',' ','X','X','X',' ',' ','X'},
+				{'X',' ','I',' ','I',' ','X',' ',' ','X'},
+				{'X','X','X',' ','X','X','X',' ',' ','X'},
+				{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X','X','X',' ','X','X','X','X',' ','X'},
+				{'X',' ','I',' ','I',' ','X','k',' ','X'},
+				{'X','X','X','X','X','X','X','X','X','X'}};
+
+		char[][] ogreMap = {{'X','X','X','X','X','X','X','X','X'},
+				{'I',' ',' ',' ','0',' ',' ','k','X'},
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X','H',' ',' ',' ',' ',' ',' ','X'},
+				{'X','X','X','X','X','X','X','X','X'}};
+
+		//boolean finish = levelOne(a);
+		boolean finish = true;
 		if (finish == true) {
-			System.out.println("Ganhei o jogo, e acabei de perder o jogo.");
+			System.out.println("You escaped! Good work!\n But wait...\n Oh no, there's an ogre on the loose!\nReach the key and escape!\n\n");
+			isLevelTwo = true;
 		}
-		if (!finish) 
-			System.out.println("Acabei efetivamente de perder o jogo.");
+		if (!finish){
+			System.out.println("You got caught! Try again!");
+			return;
+		}
+		boolean finishLevelTwo = levelTwo(ogreMap);
+		if (finishLevelTwo == true) {
+			System.out.println("You have finished the game! Thanks for playing!");
+		}
+		if (!finishLevelTwo){
+			System.out.println("You got caught! Try again!");
+			return;
+		}
+
 	}
 
-	public static boolean game(char[][] a) {
-		boolean won = false, lost = false;;
+	public static boolean levelOne(char[][] a) {
+		boolean won = false, lost = false;
 		Scanner scan = new Scanner(System.in);
-		
+
 		showMap(a);
 		do {
 			a = move(a, scan.next().charAt(0));
-			
+
 			if (a[5][0] == 'H' || a[6][0] == 'H')
 				won = true;
 			if (checkPresence(a)) {
 				lost = true;
 				won = false;
 			}
-			
+
 		} while (!won && !lost);
 		return won;
 	}
-	
+
+	public static boolean levelTwo(char[][] ogreMap) {
+		boolean won = false, lost = false;
+		Scanner scan = new Scanner(System.in);
+
+		showMap(ogreMap);
+		do {
+			ogreMap = move(ogreMap, scan.next().charAt(0));
+
+			if (ogreMap[1][0] == 'K')
+				won = true;
+			if (checkPresence(ogreMap)) {
+				lost = true;
+				won = false;
+			}
+
+		} while (!won && !lost);
+		return won;
+	}
+
 	public static boolean checkPresence(char[][] a) {
 		int row = 0, col = 0, rowg = 0,colg = 0;
 		for (int i = 0 ; i < a.length; i++) {
 			for (int j = 0; j < a[i].length; j++) {
-				if (a[i][j] == 'H') {
+				if (a[i][j] == 'H' || a[i][j] == 'K') {
 					row = i; col = j;
 				}
-				if (a[i][j] == 'G') {
+				if (a[i][j] == 'G' || a[i][j] == '0') {
 					rowg = i;
 					colg = j;
 				}
 			}
 		}
-		
-		if (colg == col && (rowg == row-1 || rowg == rowg+1 ))
+
+		if (colg == col && (rowg == row-1 || rowg == row+1 ))
 			return true;
 		if (rowg == row && (colg == col-1 || colg == col+1 ))
 			return true;
 		return false;
 	}
-	
+
 	public static char[][] move(char[][] a, char c) {
+		char s;
 		boolean won = false;
 		int row = 0, col = 0;
 		for (int i = 0 ; i < a.length; i++) {
 			for (int j = 0; j < a[i].length; j++) {
-				if (a[i][j] == 'H') {
+				if (a[i][j] == 'H' || a[i][j] == 'K') {
 					row = i;
 					col = j;
 					j = a[i].length-1;
@@ -68,63 +126,69 @@ public class dungeonEscape {
 			}
 		}
 		char result;
-		int x;
-		if (c == 'u') 
+		if (c == 'w') 
 			result = next(a[row-1][col]);
-
-
-		else if (c == 'l')
+		else if (c == 'a')
 			result = next(a[row][col-1]);
-
-		else if (c == 'd') 
+		else if (c == 's') 
 			result = next(a[row+1][col]);
-		else if (c == 'r')
+		else if (c == 'd')
 			result = next(a[row][col+1]);
 		else 
 			result = 'N';
 
 		if (result == 'H') {
-			if (c == 'u') {
+			if (c == 'w') {
 				a[row][col] = ' ';
 				a[row-1][col] = 'H';
 			}
 
-			else if (c == 'l') {
+			else if (c == 'a') {
 				a[row][col] = ' ';
 				a[row][col-1] = 'H';
 			}
-			
-			
-			else if (c == 'd') {
+
+			else if (c == 's') {
 				a[row][col] = ' ';
 				a[row+1][col] = 'H';
 			}
-			else if (c == 'r') {
+			else if (c == 'd') {
 				a[row][col] = ' ';
 				a[row][col+1] = 'H';
 			}
 
 		}
-		
-		if (result == 'S') {
-			
 
-			if (c == 'l' && col > 1) {
+		if (result == 'S') {
+			if (c == 'a' && col > 1) {
 				a[row][col] = ' ';
 				a[row][col-2] = 'H';
 			}
-			else if (c == 'l' && col == 1) {
+			else if (c == 'a' && col == 1) {
 				a[row][col] = ' ';
 				a[row][col-1] = 'H';
 			}
-			else if (c == 'r') {
+			else if (c == 'd') {
 				a[row][col] = ' ';
 				a[row][col+2] = 'H';
 			}
-			
+
 		}
 
 		if (result == 'E') {
+
+			if(isLevelTwo){
+				if(c == 'w'){
+					a[row][col] = ' ';
+					a[row-1][col] = 'K';
+				} else if(c == 'd'){
+					a[row][col] = ' ';
+					a[row][col+1] = 'K';
+				}
+
+
+			}
+
 			for (int i = 0; i < a.length; i++) {
 				for (int j = 0; j < a[i].length; j++) {
 					if (a[i][j] == 'I')
@@ -132,12 +196,104 @@ public class dungeonEscape {
 				}
 			}
 		}
-		a = moveGuard(a);
+
+		if(isLevelTwo){
+			a = moveOgre(a);
+		} else {
+			a = moveGuard(a);
+		}
 		showMap(a);
 		return a;
 
 	}
-	
+
+	public static char[][] moveOgre(char[][] a) {
+
+		String s = "wasd";
+		boolean restoreKey = false;
+		Random rand =  new Random();
+		int index = rand.nextInt(s.length());
+		char c =  s.charAt(index);
+		char result;
+
+		int row = 0, col = 0;
+		for (int i = 0 ; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				if (a[i][j] == '0') {
+					row = i;
+					col = j;
+				}
+			}
+		}
+
+		System.out.println("Ogre: " + c);
+
+		if (c == 'w'){
+			result = next(a[row-1][col]);
+			System.out.println(result);
+		} else if (c == 'a'){
+			result = next(a[row][col-1]);
+			System.out.println(result);
+		} else if (c == 's') {
+			result = next(a[row+1][col]);
+			System.out.println(result);
+		} else if (c == 'd'){
+			result = next(a[row][col+1]);
+			System.out.println(result);
+		} else {
+			result = 'N';
+		}
+
+		if (result == 'H') {
+			if (c == 'w') {
+				a[row][col] = ' ';
+				a[row-1][col] = '0';
+			}
+
+			else if (c == 'a') {
+
+				if(restoreKey){
+					a[1][7] = 'k';
+					restoreKey = false;
+				} else {
+					a[row][col] = ' ';
+				}
+				a[row][col-1] = '0';
+			}
+
+
+			else if (c == 's') {
+
+				if(restoreKey){
+					a[1][7] = 'k';
+					restoreKey = false;
+				} else {
+					a[row][col] = ' ';
+				}
+				a[row+1][col] = '0';
+			}
+			else if (c == 'd') {
+				a[row][col] = ' ';
+				a[row][col+1] = '0';
+			}
+
+		}
+
+		if(result == 'E'){
+			if(c == 'w'){
+				a[row][col] = ' ';
+				a[row-1][col] = '$';
+			} else if(c == 'd'){
+				a[row][col] = ' ';
+				a[row][col+1] = '$';
+			}
+			restoreKey = true;
+		}
+
+		return a;
+
+	}
+
 	public static char [][] moveGuard(char[][] a) {
 		int rowg = 0, colg = 0;
 		for (int i = 0 ; i < a.length; i++) {
@@ -148,7 +304,7 @@ public class dungeonEscape {
 				}
 			}
 		}
-		
+
 		if (rowg == 1 && colg == 8) {
 			a[rowg][colg] = ' ';
 			a[rowg][colg-1] = 'G';
@@ -173,29 +329,25 @@ public class dungeonEscape {
 			a[rowg][colg] = ' ';
 			a[rowg-1][colg] = 'G';
 		}
-			
-		
+
+
 		return a;
-		
+
 	}
 
 
 	public static char next(char c) {
 		if (c == 'X')
 			return 'X';
-		else if (c == 'K')
+		else if (c == 'k')
 			return 'E';
 		else if (c == ' ') 
 			return 'H';
 		else if (c == 'I')
 			return 'X';
-		else if (c == 'S') {
+		else if (c == 'S'){
 			return 'S';
 		}
-		return c;
-	}
-
-	public static char[][] changePosition(char[][] c) {
 		return c;
 	}
 
