4bf1b6ecee70788af3540d0cfbc338ea179a8bb6
Diogo Dores <diogo.dores@gmail.com>
2017-02-21 15:36:51 +0000

    +1 Package

---
+++ TP1 & Dragons/src/dkeep/cli/UserInterface.java
@@ -0,0 +1,17 @@
+package dkeep.cli;
+
+public class UserInterface {
+	
+	public static void main(String[] args){
+	
+		
+		
+	}
+	
+	public static void showMap(char[][] a) {
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+		System.out.println("\n");
+	}
+}
