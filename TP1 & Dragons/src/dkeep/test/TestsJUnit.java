5c6fec575b99e41646206634ba8cb59bbcee8661
Pedro Daniel dos Santos Reis <petrexkgb@gmail.com>
2017-03-05 14:57:29 +0000

    Testes começam a funcionar, tenho que tratar do resto

--- TP1 & Dragons/src/dkeep/test/TestsJUnit.java
+++ TP1 & Dragons/src/dkeep/test/TestsJUnit.java
@@ -27,18 +27,8 @@ public class TestsJUnit {
 		game.hero.move(testMap, 's');
 		assertEquals(2, game.hero.getX());
 		assertEquals(2, game.hero.getY());
-		System.out.println(game.currentMap.getName());
-		for (int i = 0 ; i < map.length ; i++) {
-			for (int j = 0 ; j < map[i].length; j++) {
-				System.out.println(map[i][j]);
-			}
-		}
-		//System.out.println(map[1][0]);
-
-		//assertEquals(map[1][1], map[game.hero.getX()][game.hero.getY()]);
-		//System.out.println(map[1][2]);
-		//assertEquals('H', map[1][2]);
-		//game.createGuard(3,1);		
+		
+		game.createGuard(3,1);		
 	
 		
 	}
