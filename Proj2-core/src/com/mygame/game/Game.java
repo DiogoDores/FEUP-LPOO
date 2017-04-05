e484730ad1c0e8df34ca87f2835e112d17522db0
Diogo Dores <diogo.dores@gmail.com>
2017-04-05 14:59:28 +0100

    ,

--- Proj2-core/src/com/mygame/game/Game.java
+++ Proj2-core/src/com/mygame/game/Game.java
@@ -1,4 +1,4 @@
-package com.mygdx.game;
+package com.mygame.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class PrairieKing extends ApplicationAdapter {
+public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
@@ -16,7 +16,7 @@ public class PrairieKing extends ApplicationAdapter {
 		img = new Texture("badlogic.jpg");
 	}
 
-	@Override 
+	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
