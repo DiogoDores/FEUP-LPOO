18573a29ea822a711821e1b24ebab38b8301feab
Pedro Daniel dos Santos Reis <petrexkgb@gmail.com>
2017-04-09 21:24:48 +0100

    Revert ","
    This reverts commit e484730ad1c0e8df34ca87f2835e112d17522db0.

--- prairie-king-core/src/com/mygdx/game/PrairieKing.java
+++ prairie-king-core/src/com/mygdx/game/PrairieKing.java
@@ -1,4 +1,4 @@
-package com.mygame.game;
+package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Game extends ApplicationAdapter {
+public class PrairieKing extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
@@ -16,7 +16,7 @@ public class Game extends ApplicationAdapter {
 		img = new Texture("badlogic.jpg");
 	}
 
-	@Override
+	@Override 
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
