280040e8d33ac317896eac9e5d265c2bdea36822
Pedro Daniel dos Santos Reis <petrexkgb@gmail.com>
2017-04-20 15:17:57 +0100

    Comecei o Gameflow

---
+++ Prairie King/core/src/com/mygdx/game/GameLogic.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * Created by petre on 20/04/2017.
+ */
+
+public class GameLogic extends Game {
+
+    public GameLogic() {
+
+    }
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+
+}
