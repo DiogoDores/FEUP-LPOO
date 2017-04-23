796b29d56c5fdd39400d7f0a9c55702a4eaa92c3
Pedro Daniel dos Santos Reis <petrexkgb@gmail.com>
2017-04-23 11:54:25 +0100

    Cenas aleatorias

--- Prairie King/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ Prairie King/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -6,7 +6,6 @@ import com.mygdx.game.PrairieKing;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		System.out.print("WOW");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new PrairieKing(), config);
 	}
