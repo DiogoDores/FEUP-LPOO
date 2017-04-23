6f5461447de0d01100d720e68a011b67a8860974
Pedro Daniel dos Santos Reis <petrexkgb@gmail.com>
2017-04-23 22:56:22 +0100

    Adicionei o LoseScreen, inicio do Hero e GameLogic e melhoramentos

--- Prairie King/core/src/com/mygdx/game/GameLogic.java
+++ Prairie King/core/src/com/mygdx/game/GameLogic.java
@@ -1,15 +1,43 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
 
-/**
- * Created by petre on 20/04/2017.
- */
+public class GameLogic extends Stage {
 
-public class GameLogic {
+    Hero hero;
 
+    public GameLogic() {
+        hero = new Hero(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight());
+    }
 
+    @Override
+    public void draw() {
+        super.draw();
+    }
 
+    @Override
+    public void act() {
+        super.act();
+    }
+
+    @Override
+    public boolean keyDown(int keyCode) {
+        return super.keyDown(keyCode);
+    }
+
+    @Override
+    public void addActor(Actor actor) {
+        super.addActor(actor);
+    }
+
+    @Override
+    public Array<Actor> getActors() {
+        return super.getActors();
+    }
 }
