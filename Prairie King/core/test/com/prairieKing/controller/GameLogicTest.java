bcfc27affee255eeeae82a035f80f08904f00209
Diogo Dores <diogo.dores@gmail.com>
2017-06-05 21:17:20 +0100

    Oh boi it's working

---
+++ Prairie King/core/test/com/prairieKing/controller/GameLogicTest.java
@@ -0,0 +1,13 @@
+package com.prairieKing.controller;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Utilizador on 05/06/2017.
+ */
+public class GameLogicTest extends GameTest{
+
+
+
+
+}
--- Prairie King/core/test/com/prairieKing/controller/GameTest.java
+++ Prairie King/core/test/com/prairieKing/controller/GameTest.java
@@ -1,19 +1,29 @@
-package com.prairieKing.test;
-
+package com.prairieKing.controller;
 
 import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.graphics.GL20;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.mockito.Mockito;
+
+/**
+ * Created by Utilizador on 05/06/2017.
+ */
 
 public class GameTest {
-
     // This is our "test" application
     private static Application application;
 
     // Before running any tests, initialize the application with the headless backend
-    //@org.junit.BeforeClass
+    @BeforeClass
     public static void init() {
         // Note that we don't need to implement any of the listener's methods
-        //application = new HeadlessApplication(new ApplicationListener() {
-          /* @Override public void create() {}
+        application = new HeadlessApplication(new ApplicationListener() {
+            @Override public void create() {}
             @Override public void resize(int width, int height) {}
             @Override public void render() {}
             @Override public void pause() {}
@@ -23,11 +33,11 @@ public class GameTest {
 
         // Use Mockito to mock the OpenGL methods since we are running headlessly
         Gdx.gl20 = Mockito.mock(GL20.class);
-        Gdx.gl = Gdx.gl20;*/
+        Gdx.gl = Gdx.gl20;
     }
 
     // After we are done, clean up the application
-    //@AfterClass
+    @AfterClass
     public static void cleanUp() {
         // Exit the application first
         application.exit();
